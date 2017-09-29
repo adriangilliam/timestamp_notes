f3079a264e88f2e0243386438c03a1cb533d8377
Lindsay Verola <lindsayv@squareup.com>
2017-09-29 16:12:32 -0400

    Add pros and cons

--- js/notes.js
+++ js/notes.js
@@ -1,28 +1,60 @@
 $(function() {
     $('#notes-form').on('submit', function() {
-    	let noteText = $('#note-text').val();
-    	if (noteText == undefined || noteText == '') {
-    		return false
-    	}
-	    takeNote(noteText);
-	    $('#note-text').val('');
-	    return false;
-	});
+      let noteText = $('#note-text').val();
+      if (noteText != undefined && noteText != '') {
+        takeNote(noteText);
+        $('#note-text').val('');
+      }
 
-	$('#clearBtn').on('click', function() {
-		clearNotes();
-	})
+      let proText = $('#pros-text').val();
+      if (proText != undefined && proText != '') {
+        recordPro(proText, '(+)');
+        $('#pros-text').val('');
+      }
 
-	if (localStorage.notes != undefined) {
-		$('#notes').html(localStorage.notes);
-	}
+      let conText = $('#cons-text').val();
+      if (conText != undefined && conText != '') {
+        recordCon(conText, '(-)');
+        $('#cons-text').val('');
+      }
+
+      return false;
+  });
+
+  $('#clearBtn').on('click', function() {
+    clearNotes();
+  })
+
+  if (localStorage.notes != undefined) {
+    $('#notes').html(localStorage.notes);
+  }
+
+  if (localStorage.pros != undefined) {
+    $('#pros').html(localStorage.notes);
+  }
+
+  if (localStorage.cons != undefined) {
+    $('#cons').html(localStorage.notes);
+  }
 });
 
 function takeNote(text) {
-	let timeStamp = getTimeStamp();
-	let note = "<li><b>" + timeStamp + "</b> - " + text + "</li>";
-	$('#notes').append(note);
-	localStorage.notes = $('#notes').html();
+  let timeStamp = getTimeStamp();
+  let note = "<li><b>" + timeStamp + "</b> - " + text + "</li>";
+  $('#notes').append(note);
+  localStorage.notes = $('#notes').html();
+}
+
+function recordPro(text) {
+  let note = "<li> (+) " + text + "</li>";
+  $('#pros').append(note);
+  localStorage.pros = $('#pros').html()
+}
+
+function recordCon(text) {
+  let note = "<li> (-) " + text + "</li>";
+  $('#cons').append(note);
+  localStorage.cons = $('#cons').html()
 }
 
 function getTimeStamp() {
@@ -30,9 +62,12 @@ function getTimeStamp() {
 }
 
 function clearNotes() {
-	let response = confirm("Are you sure you want to clear your notes?");
-	if (response) {
-    	localStorage.notes = '';
-		$('#notes').html('');
-	}
+  let response = confirm("Are you sure you want to clear your notes?");
+  if (response) {
+      localStorage.notes = '';
+      localStorage.prosAndCons = '';
+    $('#notes').html('');
+    $('#pros').html('');
+    $('#cons').html('');
+  }
 }
