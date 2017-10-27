0e2f9048ddc66683507cf51541f145aa6c37aa21
Lindsay Verola <lindsayv@squareup.com>
2017-10-27 11:51:26 -0400

    Add support for pros cons and mehs

--- js/notes.js
+++ js/notes.js
@@ -1,28 +1,54 @@
+const FIELDS = ['pros', 'cons', 'mehs', 'overview', 'notes']
+
+const SPECIAL_CHARACTERS = {
+    '+': 'pros',
+    '-': 'cons',
+    '~': 'mehs',
+    '=': 'overview',
+}
+
+const PREFIXES = {
+    '+': '(+) ',
+    '-': '(-) ',
+    '~': '(~) ',
+    '=': '',
+};
+
 $(function() {
     $('#notes-form').on('submit', function() {
-    	let noteText = $('#note-text').val();
-    	if (noteText == undefined || noteText == '') {
-    		return false
-    	}
-	    takeNote(noteText);
-	    $('#note-text').val('');
-	    return false;
-	});
+        let noteText = $('#note-text').val();
+        if (noteText == undefined || noteText == '') {
+            return false;
+        }
+        takeNote(noteText);
+        $('#note-text').val('');
+        return false;
+    });
 
-	$('#clearBtn').on('click', function() {
-		clearNotes();
-	})
+  $('#clearBtn').on('click', function() {
+      clearNotes();
+  })
 
-	if (localStorage.notes != undefined) {
-		$('#notes').html(localStorage.notes);
-	}
+  FIELDS.forEach(function(field) {
+      if (localStorage[field] != undefined) {
+          $(`#${field}`).html(localStorage[field]);
+      }
+  });
 });
 
 function takeNote(text) {
-	let timeStamp = getTimeStamp();
-	let note = "<li><b>" + timeStamp + "</b> - " + text + "</li>";
-	$('#notes').append(note);
-	localStorage.notes = $('#notes').html();
+    if(text[0] in SPECIAL_CHARACTERS) {
+        record(SPECIAL_CHARACTERS[text[0]], text.substr(1), PREFIXES[text[0]]);
+    } else {
+        record('notes', text, "<b>" + getTimeStamp() + "</b> - ");
+    }
+}
+
+function record(field, text, prefix) {
+    let note = "<li>" + prefix + text + "</li>";
+    const elem = $(`#${field}`)
+    elem.append(note);
+    localStorage[field] = elem.html();
 }
 
 function getTimeStamp() {
@@ -30,9 +56,12 @@ function getTimeStamp() {
 }
 
 function clearNotes() {
-	let response = confirm("Are you sure you want to clear your notes?");
-	if (response) {
-    	localStorage.notes = '';
-		$('#notes').html('');
-	}
+    let response = confirm("Are you sure you want to clear your notes?");
+    if (response) {
+        FIELDS.forEach(function(field) {
+            localStorage[field] = '';
+            $(`#${field}`).html('');
+        });
+    }
 }
+
