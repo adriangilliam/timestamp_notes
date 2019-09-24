4ee2ee94d6aed182cc7d8e9883227644f8cef8db
Lindsay Verola <lindsayv@squareup.com>
2019-09-24 11:48:15 -0400

    Add support for eng rubric

--- js/notes.js
+++ js/notes.js
@@ -1,18 +1,44 @@
 const FIELDS = ['pros', 'cons', 'mehs', 'overview', 'notes']
 
 const SPECIAL_CHARACTERS = {
+    '++': 'pros',
     '+': 'pros',
+    '--': 'cons',
     '-': 'cons',
     '~': 'mehs',
     '=': 'overview',
 }
 
 const PREFIXES = {
-    '+': '(+) ',
-    '-': '(-) ',
-    '~': '(~) ',
+    '+': '[+] ',
+    '++': '[++] ',
+    '-': '[-] ',
+    '--': '[--]',
+    '~': '[~] ',
     '=': '',
 };
+
+const AVAILBLE_DIMENSIONS = [
+    'Analysis',
+    'Troubleshooting',
+    'Code Fluency',
+    'Code Maintainability',
+    'Technical Design',
+    'Domain Specific Knowledge',
+    'Curiosity',
+    'Awareness',
+    'Mentorship',
+    'Empathy',
+    'Communciation',
+    'Collaboration',
+    'Community Building',
+    'Team Effectiveness',
+    'Economoic Empowerment',
+    "Understanding Someone's Struggle",
+    "Seeking Divergent Perspectives",
+    "Taking Principled Risks",
+    "Inventing and Learning",
+];
 
 $(function() {
     $('#notes-form').on('submit', function() {
@@ -43,11 +69,37 @@ $(function() {
         $('#savedNotes').html(localStorage.savedNotes);
     }
 
+    $( "#note-text" ).autocomplete({
+        autoFocus:true,
+        minLength: 3,
+        source: function(request, response) {
+
+            var regex = new RegExp(/\+*\-*\s*/, 'g')
+            regex.test(request.term);
+            var prefix = request.term.substr(0, regex.lastIndex);
+            var possibleDimension = request.term.replace(/\+*\-*\s*/, '')
+
+            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(possibleDimension), 'i' );
+            response($.grep( AVAILBLE_DIMENSIONS, function( value ) {
+              return matcher.test( value.label || value.value || value) ;
+            }).map(function(value) { return prefix + " " + value + " "; }));
+        }
+    });
+
+     $("#note-text").keydown(function(event) {
+        if(event.keyCode != $.ui.keyCode.TAB) { return true; }
+        $(this).trigger($.Event('keydown'), { keyCode: $.ui.keyCode.ENTER });
+        $(this).autocomplete( "close" );
+        return false;
+    });
 });
 
 function takeNote(text) {
-    if(text[0] in SPECIAL_CHARACTERS) {
-        record(SPECIAL_CHARACTERS[text[0]], text.substr(1), PREFIXES[text[0]]);
+    var specialChar = Object.keys(SPECIAL_CHARACTERS).find(function(key) {
+        return text.startsWith(key);
+    });
+    if(specialChar) {
+        record(SPECIAL_CHARACTERS[specialChar], text.substr(specialChar.length), PREFIXES[specialChar]);
     } else {
         record('notes', text, "<b>" + getTimeStamp() + "</b> - ");
     }
