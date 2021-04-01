44f68eab04f7932a57dcfc60e977bfc0498b02e4
Lindsay Verola <lindsayv@squareup.com>
2021-04-01 11:55:05 -0400

    Fix typo

--- js/notes.js
+++ js/notes.js
@@ -29,7 +29,7 @@ const AVAILBLE_DIMENSIONS = [
     'Awareness',
     'Mentorship',
     'Empathy',
-    'Communciation',
+    'Communication',
     'Collaboration',
     'Community Building',
     'Team Effectiveness',
